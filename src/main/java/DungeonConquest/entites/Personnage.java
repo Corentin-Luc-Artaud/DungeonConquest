f46454f7bccafb531aeaf7f61baf6b483675c566
Maxime Bouis <Maxime Bouis>
2016-11-11 15:13:55 +0100

    correction classpath

--- src/main/java/DungeonConquest/entites/Personnage.java
+++ src/main/java/DungeonConquest/entites/Personnage.java
@@ -4,6 +4,16 @@ public class Personnage extends EntiteDynamique{
 
 	private double pointsDeVie;
 	
+	private Equipement casque;
+	private Equipement plastron;
+	private Equipement bottes;
+	private Equipement gants;
+	
+	private Equipement collier;
+	private Equipement bague;
+	
+	private Equipement arme;
+	
 	@Override
 	public boolean estEnVie() {
 
@@ -11,6 +21,18 @@ public class Personnage extends EntiteDynamique{
 			return true;
 		
 		return false;
+	}
+	
+	public void equiperCasque (Casque casque) {
+		this.casque = casque;
+	}
+	
+	public void equiperPlastron (Plastron plastron) {
+		this.plastron = plastron;
+	}
+	
+	public void equiperBottes (Bottes bottes) {
+		this.bottes = bottes;
 	}
 
 }
