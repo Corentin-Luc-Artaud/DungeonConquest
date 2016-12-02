f239abd0d02eed349c6900878ed9dc3bada62668
Nathan Bernard <Nathan Bernard>
2016-12-02 11:09:44 +0100

    essaie du hub sur l'interface avec les textures

--- src/test/java/Test/testtexture.java
+++ src/test/java/Test/testtexture.java
@@ -1,18 +1,24 @@
 package Test;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
-import DungeonConquest.PanelFond;
+import Test.PanelFond;
 
 public class testtexture extends JFrame {
+    static final int taille = 32*32;
 	public static void main( String[] args )
     {
 		//fenêtre du jeu
@@ -21,10 +27,29 @@ public class testtexture extends JFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         //grille de jeu
-        JPanel panneaudejeu = new JPanel();
-        panneaudejeu.setLayout(new GridLayout(32,32));
+        JPanel panneautuille = new JPanel();
+        panneautuille.setLayout(new GridLayout(taille/32,taille/32));
+      
         
-
+        //fenêtre 
+        JPanel interfacejeu = new JPanel(new BorderLayout());
+        JPanel panneauhub = new JPanel(new GridLayout(1,3));
+        for( int  i = 0; i<3 ; ++i)
+        {
+        	JButton bouton = new JButton("salut");
+        	bouton.setSize(new Dimension(40,25));
+        	panneauhub.add(bouton);
+        }
+        JPanel panneaudroite = new JPanel(new GridLayout(3,1));
+        for( int  i = 0; i<3 ; ++i)
+        {
+        	JButton bouton = new JButton("salut");
+        	bouton.setSize(new Dimension(40,80));
+        	panneaudroite.add(bouton);
+        }
+        
+        
+        
         // vecteur des images
         ImageIcon sol = new ImageIcon("C:/Users/Nathan/Desktop/workspace/DungeonConquest/assets/Tuille/Donjon/pavé1.png");
         ImageIcon mur = new ImageIcon("C:/Users/Nathan/Desktop/workspace/DungeonConquest/assets/Tuille/Donjon/mur1.png");
@@ -35,7 +60,7 @@ public class testtexture extends JFrame {
         
         //matrice de correspondance
         ArrayList<Integer> matrice = new ArrayList<Integer>();
-        for(int i = 0 ; i< 32*32; i=i+1)
+        for(int i = 0 ; i< taille; i=i+1)
         {
         	if(i<32 || i%32==0 || (i+1)%32==0 || i>32*32-32)
         		matrice.add(i, 1);
@@ -44,23 +69,31 @@ public class testtexture extends JFrame {
         }
         
         
-        
-        for(int i = 0 ; i<32*32; i++)
+        //insertation des images dans le grid layout en fonction de la matrice de correspondance des images et celle du plateau
+        for(int i = 0 ; i<taille; i++)
         {
         	if(matrice.get(i)==1)
-        	{	
-        		JLabel lab = new JLabel("1");
-        		panneaudejeu.add(new PanelFond(vecteurimage.get(1)));
+        	{
+        		PanelFond tmur = new PanelFond(vecteurimage.get(1));
+        		tmur.resizeFond(32,32);
+        		panneautuille.add(tmur);
         	}
         	else if(matrice.get(i)==0)
         	{
-        		JLabel lab = new JLabel("0");
-
-    			panneaudejeu.add(new PanelFond(vecteurimage.get(0)));
+        		PanelFond tsol = new PanelFond(vecteurimage.get(0));
+        		tsol.resizeFond(32,32);
+        		panneautuille.add(tsol);        
         	}
         }   
         
-        frame.add(panneaudejeu);
+        
+        //les ajouts sur le fenêtre principale
+        
+        interfacejeu.add(panneautuille,BorderLayout.CENTER);
+        interfacejeu.add(panneauhub, BorderLayout.NORTH);
+        interfacejeu.add(panneaudroite, BorderLayout.EAST);
+
+        frame.add(interfacejeu);
         frame.setVisible(true);
         frame.pack();
     }
