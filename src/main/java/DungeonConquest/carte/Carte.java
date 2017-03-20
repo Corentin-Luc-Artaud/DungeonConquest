89b1c6b817d7650b3500934c5e158414773d98f2
Yacine Battis <nalfein84@gmail.com>
2017-03-20 08:56:57 +0100

    ajout de la fonction de génération de la carte

--- src/main/java/DungeonConquest/carte/Carte.java
+++ src/main/java/DungeonConquest/carte/Carte.java
@@ -1,8 +1,13 @@
 package DungeonConquest.carte;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import DungeonConquest.entites.statiques.cases.Case;
+import DungeonConquest.entites.statiques.cases.Mur;
+import DungeonConquest.entites.statiques.cases.Sol;
 import DungeonConquest.entites.utilitaire.Coordonnee;
 
 public class Carte {
@@ -19,6 +24,30 @@ public class Carte {
 	
 	public static Carte getInstance() {
 		return CARTE;
+	}
+	
+	public void generer(String filename) {
+		File file = new File(filename);
+		if(file.exists() && file.canRead()){
+			try {
+				Scanner in = new Scanner(file);
+				for (int i = 0; i < matCase.size(); i++) {
+					for (int j = 0; j < matCase.get(i).size(); j++) {
+						String str = in.next();
+						//////////////////////////////////
+						// TODO A améliorer
+						if (str.equals("#")) matCase.get(i).set(j, new Mur(str, null));
+						else if(str.equals("1")) matCase.get(i).set(j, new Sol(str, null, null));
+						//////////////////////////////////
+					}
+				}
+				in.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 	}
 	
 	public void addCase(Case maCase, Coordonnee coordonnee) {
